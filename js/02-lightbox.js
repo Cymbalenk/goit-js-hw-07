6d0f6a8bd8d479470b0e0020b18717c50a006597
Cymbalenk <111699267+Cymbalenk@users.noreply.github.com>
2023-02-19 09:50:17 +0200

    Add CSS and JS

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,24 +1,4 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const gallerySection = document.querySelector(".gallery");
-const galleryMarkUp = createGalleryMarkUp(galleryItems);
 
-gallerySection.insertAdjacentHTML("beforeend", galleryMarkUp);
-
-function createGalleryMarkUp(galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `
-            <a class="gallery__item" href="${original}">
-  <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`;
-    })
-    .join("");
-} 
-
-
-  var lightbox = new SimpleLightbox(".gallery__item", {
-    captionDelay: 250,
-    captionData: "alt",
-  });
-
+console.log(galleryItems);
